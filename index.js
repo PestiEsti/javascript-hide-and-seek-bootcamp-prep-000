2f9cac505b61b24035dbf6c88f99620d1360781a
Steven Luck <steve.luck@gmail.com>
2020-03-26 13:00:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector) {
+  let response = document.querySelector(selector)
+  return(response)
+}
 
+function nestedTarget() {
+  let response = document.querySelector('#nested .target')
+  return(response)
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+ 
+  for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = parseInt(lis[i].innerHTML)+n;
+  }
+}
