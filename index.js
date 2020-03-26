292d7d05a9b208819769e7131dec7b0d71bdccef
Steven Luck <steve.luck@gmail.com>
2020-03-26 14:09:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  let response = document.querySelector(selector)
+  return(response)
+}
 
+function nestedTarget() {
+  let response = document.querySelector('#nested .target')
+  return(response)
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+ 
+  for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = parseInt(lis[i].innerHTML)+n;
+  }
+}
+
+function deepestChild() {
+  let levels = document.getElementById('grand-node').querySelectorAll('div');
+  let response = levels[levels.length-1];
+  return(response);
+}
